474ab39b6f45cf0292cc70c82e83576b26afbbc6
DMurashko <appleitunesstore590@gmail.com>
2020-02-27 15:13:51 +0200

    I have done 4 tasks

---
+++ ft_is_negative.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void ft_is_negative (int num) {
+	scanf ("%d", %num);
+	if (num >= 0) {
+	printf("P");
+	} else { 
+	printf("N");
+	}
+}
+
+int main () {
+	ft_is_negative ();
+	return 0;
+}
